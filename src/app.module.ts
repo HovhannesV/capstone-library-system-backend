c00b057c3158ede8893ec8b7e9544ab95912ff4b
hv <hovhannes.vardanyan@picsart.com>
2022-03-14 00:49:03 +0400

    adding more module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import {MongooseModule} from "@nestjs/mongoose";
 import {FileModule} from "./file/file.module";
 import {APP_GUARD} from "@nestjs/core";
 import {AuthGuard} from "./user/auth.guard";
+import {CoverTypeModule} from "./coverType/cover-type.module";
+import {GenreModule} from "./genre/genre.module";
 
 @Module({
     imports: [
         UserModule,
         AuthorModule,
+        CoverTypeModule,
+        GenreModule,
         MongooseModule.forRoot(
             process.env.MONGO_CONN_STRING,
             {
